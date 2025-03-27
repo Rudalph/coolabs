e2d711b074b5dbe851caf3528e6679e593b6ed89
Rudalph <gonsalvesrudalph@gmail.com>
2025-03-27 22:14:08 +0530

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "../context/AuthContext"
