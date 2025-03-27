a3322315c37bfe77e49cb639ececa5bc83c9b5da
Rudalph <gonsalvesrudalph@gmail.com>
2025-03-27 22:22:10 +0530

    metadata updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-// import type { Metadata } from "next";
+import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "../context/AuthContext"
@@ -9,6 +9,12 @@ const spaceGrotesk = Space_Grotesk({
   variable: "--font-space-grotesk",
   subsets: ["latin"],
 });
+
+export const metadata: Metadata = {
+  title: "My Next.js App",
+  description: "This is a Next.js application with authentication.",
+  icons: "/favicon.ico", // Ensures favicon is correctly set
+};
 
 export default function RootLayout({
   children,
